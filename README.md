8632ec80dd2b85875ba4ad3de40b2b3825f4a54a
Zack Zlotnik <zackzlotnik@gmail.com>
2017-05-11 13:27:24 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# jenkins_jnlp_test
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: jenkins_jnlp_test
+# Recipe:: default
+#
+# Copyright:: 2017, Zack Zlotnik, All Rights Reserved.
